e9e192327a853e46050f30c6d0745573205eaed5
LuFrei <lucasfreitas09876@gmail.com>
2022-04-27 11:51:04 -0400

    dog model created

---
+++ db/models/dog.js
@@ -0,0 +1,23 @@
+const db = require('../db');
+const { DataTypes, Model } = require('sequelize');
+
+class Dog extends Model { /* methods... */};
+
+Dog.init({
+  name: DataTypes.STRING,
+  sex: DataTypes.STRING, ///*
+  age: DataTypes.NUMBER,
+  breed: DataTypes.STRING, ///*
+  location: DataTypes.STRING, ///* 
+  summary: DataTypes.STRING,
+  description: DataTypes.STRING,
+  imageUrl: DataTypes.STRING
+},{
+  sequelize: db,
+  timestamps: false
+})
+
+//* make these an enum? a seperate table?
+// for simplicity sake.. they are not for now.
+
+module.exports = Dog;
