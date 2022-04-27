1ccf6d08cc65dfdc1fef1988d76af0b872981566
monterojavier <monterodotjavier@gmail.com>
2022-04-27 15:11:19 -0400

    fix: db file name

--- db/db.js
+++ db/db.js
@@ -1,9 +1,9 @@
-const Sequelize = require("sequelize");
+const {Sequelize} = require("sequelize");
 const path = require("path");
 
 const db = new Sequelize({
   dialect: "sqlite",
-  storage: path.join(__dirname, "db.pawdoption"),
+  storage: path.join(__dirname, "db.sqlite"),
   logging: false,
 });
 
