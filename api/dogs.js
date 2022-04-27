1e3a0d272ee723bfd53af2c44745acb1fc78817e
Eric Davenport <ericdavenport@pursuit.org>
2022-04-27 16:14:59 -0400

    feat: dog api routes

--- api/dogs.js
+++ api/dogs.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const bcrypt = require("bcrypt");
 
 // These get imported from the index.js file in the models folder
-const {} = require("../db/models");
+const { Dog } = require("../db/models");
 module.exports = router;
 
 // Routes go here, but they begin with router instead of app
@@ -13,3 +13,79 @@ module.exports = router;
 // also the routes in this file already begins with dogs
 // e.g. router.get("/") == router.get("/dogs")
 // write it like this ^
+
+// Add a new dog
+router.post("/", async (req, res) => {
+  try {
+    const name = req.body.name;
+    const sex = req.body.sex;
+    const age = req.body.age;
+    const breed = req.body.breed;
+    const imageURL = req.body.imageURL;
+
+    let newDog = await Dog.create({
+      name: name,
+      sex: sex,
+      age: age,
+      breed: breed,
+      summary: req.body.summary,
+      description: req.body.description,
+      imageURL: imageURL
+    });
+
+    res.json(newDog)
+  } catch (error) {
+    res.sendStatus(500);
+  }
+
+});
+
+// read all dogs
+router.get("/", async (req, res) => {
+  try {
+    let allDogs = await Dog.findAll();
+    res.json(allDogs);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+// read single dog
+router.get("/:id", async (req, res) => {
+  try {
+    let dog = await Dog.findByPk(req.params.id);
+    res.json(dog)
+  } catch (error) {
+    res.sendStatus(500);
+  }
+
+});
+
+// update dogs --- unsure the strngth  of this method
+// name, sex, age, breed, summary(short view), description(full view), imageURL
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedInfo = req.body;
+    const id = req.params.id;
+
+    const dog = Dog.update(updatedInfo, { 
+      where: {id: id} 
+    })
+    res.json(dog)
+  } catch (error) {
+    res.sendStatus(500);
+  }
+
+});
+
+// Delete dog or change dog to adopted
+router.delete("/:id", async (Req, res) => {
+  try {
+    let dog = await Dog.findByPk(req.params.id);
+    await dog.destroy();
+    res.send("Dog has been removed from database")
+  } catch (error) {
+    res.sendStatus(500);
+  }
+
+});
